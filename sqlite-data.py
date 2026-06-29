e8b79d8f0704580755cb99e81e2112efc622bc08
Leon <office.ms.1387@gmail.com>
2026-06-30 01:20:39 +0330

    added  a new func , cleaned up other functions
    made all of the code into functions
    
    added a gather func

--- sqlite-data.py
+++ sqlite-data.py
@@ -1,38 +1,62 @@
-import sqlite3
-
-con = sqlite3.connect("accounts.db")
-cursor = con.cursor()
-
 def insert_data_accounts(usr,pas):
+    import sqlite3
+    con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
     try:
         cursor.execute("INSERT INTO accounts (username,password) VALUES (?,?)",[usr,pas])
         con.commit()
+        con.close()
     except:
         con.rollback()
         print("Something happened, rolling back!!")
+        con.close()
 
-def delete_data_accounts(id):
+def delete_data_accounts(user_id):
+    import sqlite3
+    con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
     try:
-        cursor.execute("DELETE FROM accounts WHERE user_id = (?)",[id])
+        cursor.execute("DELETE FROM accounts WHERE user_id = (?)",[user_id])
         con.commit()
+        con.close()
     except:
         con.rollback()
         print("Something happened, rolling back!!")
-def create_table():
+        con.close()
+
+def create_table_accounts():
+    import sqlite3
+    con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
     cursor.execute("""CREATE TABLE accounts(
                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                    username TEXT NOT NULL UNIQUE,
                    password TEXT NOT NULL
                    )""")
+    con.commit()
+    con.close()
 
 #import_data_accounts("leoner11",1234)
 #delete_data_accounts(1)
+def display():
+    import sqlite3
+    con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM accounts")
+    items = cursor.fetchall()
+    for i in items:
+        print(i)
+        #print("a")
+    con.close()
 
-cursor.execute("SELECT * FROM ACCOUNTS")
+def get_accounts():
+    import sqlite3
+    con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM accounts")
+    items = cursor.fetchall()
+    return items
 
-items = cursor.fetchall()
-for i in items:
-    print(i)
-    print("a")
 
-
+#insert_data_accounts("leon2","1234")
+#display()
