7cb47e145c7c833ab1d256ad8814c36f33a3e8d6
Leon <office.ms.1387@gmail.com>
2026-06-30 01:43:05 +0330

    Refactor account management functions and class
    Refactor account loading and listing functions. Remove unused sqlite3 import and add string representation for Account class.

--- Login-system.py
+++ Login-system.py
@@ -1,4 +1,24 @@
-import sqlite3
+#import sqlite3
+import sqlite_data as sql
+
+sql.insert_data_accounts("leon",1234)
+
+def load_accounts():
+    """con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM accounts")
+    """
+    #return sql.get_accounts()
+    items = sql.get_accounts()
+    account_dict = {}
+    for user_id,username,password in items:
+        account_dict[user_id] = Account(user_id,username,password)
+    return account_dict
+
+def list_accounts():
+    account_dict = load_accounts()
+    for i in account_dict:
+        print(account_dict[i])
 
 class Account:
     def __init__(self,user_id,username,password):
@@ -6,8 +26,7 @@ class Account:
         self.username = username
         self.password = password
 
-    def load_accounts(self):
-        con = sqlite3.connect("accounts.db")
-        cursor = con.cursor()
-        cursor.execute("SELECT * FROM accounts")
+    def __str__(self):
+        return f"{self.user_id} : {self.username}"
 
+#print(account_dict)
