7dc21ffb1e14cf38d92febd2df85b0bd12dc3f9a
Leon <office.ms.1387@gmail.com>
2026-06-30 04:45:44 +0330

    Alot of bug fixes + 2 new funcs
    alot of bug fixes related to transaction being atomic
    
    added transaction insert data 
    added transaction table creation

--- sqlite-data.py
+++ sqlite-data.py
@@ -11,6 +11,19 @@ def insert_data_accounts(usr,pas):
         print("Something happened, rolling back!!")
         con.close()
 
+def insert_data_transaction(user_id,username,user_id2,username2,money,money_spent):
+    import sqlite3
+    con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
+    try:
+        cursor.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?)",[user_id,username,user_id2,username2,money,money_spent])
+        con.commit()
+        con.close()
+    except:
+        con.rollback()
+        con.close()
+
+
 def delete_data_accounts(user_id):
     import sqlite3
     con = sqlite3.connect("accounts.db")
@@ -29,7 +42,8 @@ def get_money(user_id):
     con = sqlite3.connect("accounts.db")
     cursor = con.cursor()
     cursor.execute("SELECT money FROM accounts WHERE user_id = ? ",[user_id])
-    money = cursor.fetchone()
+    money_tuple = cursor.fetchone()
+    money = money_tuple[0]
     con.close()
     return money
 
@@ -37,10 +51,16 @@ def change_money(user_id,money):
     import sqlite3
     con = sqlite3.connect("accounts.db")
     cursor = con.cursor()
-    cursor.execute("UPDATE accounts SET money = ? WHERE user_id = ?",[money,user_id])
-    con.close()
+    try:
+        cursor.execute("UPDATE accounts SET money = ? WHERE user_id = ?",[money,user_id])
+        con.commit()
+        con.close()
+    except:
+        print("Transaction failed !!")
+        con.rollback()
+        con.close()
 
-def pay_transaction(user_id1,user_id2,pay_amount):
+def pay_transaction(user_id1,user_id2,pay_amount,username1,username2):
     import sqlite3
     con = sqlite3.connect("accounts.db")
     cursor = con.cursor()
@@ -49,17 +69,42 @@ def pay_transaction(user_id1,user_id2,pay_amount):
     money_1 = money_1 - pay_amount
     money_2 = get_money(user_id2)
     money_2 = money_2 + pay_amount
-    try:
+    """try:
         change_money(user_id1,money_1)
         change_money(user_id2,money_2)
         con.commit()
         con.close()
     except:
         con.rollback()
+        con.close()"""
+
+    try:
+        cursor.execute("UPDATE accounts SET money = ? WHERE user_id = ?", [money_1, user_id1])
+        cursor.execute("UPDATE accounts SET money = ? WHERE user_id = ?", [money_2, user_id2])
+        cursor.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?)",[user_id1, username1, user_id2, username2, money_1, -pay_amount])
+        cursor.execute("INSERT INTO transactions VALUES (?,?,?,?,?,?)",[user_id2, username2, user_id2, username2, money_2, +pay_amount])
+        con.commit()
+        con.close()
+
+    except:
+        print("Transaction failed !!")
+        con.rollback()
         con.close()
 
 def create_transaction_table():
-    pass
+    import sqlite3
+    con = sqlite3.connect("accounts.db")
+    cursor = con.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS transactions( 
+                    user_id     INT NOT NULL,
+                    username    TEXT NOT NULL,           
+                    user_id2    INT NOT NULL,
+                    username2   TEXT NOT NULL,
+                    money       INT NOT NULL,
+                    money_spent INT NOT NULL)
+    """)
+    con.commit()
+    con.close()
 
 def create_table_accounts():
     import sqlite3
